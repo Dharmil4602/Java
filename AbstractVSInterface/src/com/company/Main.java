f48d26dee0cd179a768a1d076695216e04656f86
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-03 23:53:40 +0530

    AbstractVSInterface Tutorial Completed

--- AbstractVSInterface/src/com/company/Main.java
+++ AbstractVSInterface/src/com/company/Main.java
@@ -2,11 +2,18 @@ package com.company;
 
 interface Bicycle
 {
+    int a = 45;
     void applyBrake(int decrement);
     void speedUp(int increment);
 }
 
-class AvonCycle implements Bicycle
+interface HornokPlease
+{
+    void Blowhorn();
+    void Hornblow();
+}
+
+class AvonCycle implements Bicycle, HornokPlease
 {
     public void applyBrake(int decrement) // Interfaces Are By Default Public, so we need to declare them as public.
     {
@@ -17,11 +24,31 @@ class AvonCycle implements Bicycle
     {
         System.out.println("Hello, This Is SpeedUp");
     }
+
+    public void Blowhorn()
+    {
+        System.out.println("Blowhorn");
+    }
+    public void Hornblow()
+    {
+        System.out.println("Hornblow");
+    }
 }
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        AvonCycle avon = new AvonCycle();
+        avon.applyBrake(1);
+//        You can create properties in interfaces and get it by dot operator
+        System.out.println(avon.a);
+//        You Cannot Modify the properties that are present in interface as they are final
+//        avon.a = 50; // By assigning a new value to a will produce error
+//        System.out.println(avon.a);
+
+        avon.Blowhorn();
+        avon.Hornblow();
     }
 }
