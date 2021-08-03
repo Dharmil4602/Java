ca09b52afae46c839d273580f53c7f4480c21aa8
Dharmil Shah <dharmilshah476@gmail.com>
2021-08-04 01:21:00 +0530

    DefaultMethods Tutorial Completed, in this there is example for interface also

---
+++ DefaultMethods/src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+// Lets See Some Examples For Interfaces
+interface Camera
+{
+    void takeSnap();
+    void recordVideo();
+}
+
+interface Wifi
+{
+    String [] getNetwork();
+    void connectToNetwork(String network);
+}
+
+class myCellPhone
+{
+    void callNumber(int phone)
+    {
+        System.out.println("Calling " + phone);
+    }
+
+    void pickCall()
+    {
+        System.out.println("Incoming");
+    }
+}
+
+class mySmartPhone extends myCellPhone implements Camera, Wifi
+{
+
+    public void takeSnap() {
+        System.out.println("Taking Snap");
+    }
+
+    public void recordVideo() {
+        System.out.println("Recording Video");
+    }
+
+    public String[] getNetwork() {
+        System.out.println("Getting Networks: ");
+        String[] networkList = {"Dharmil", "Khushi", "Souravi"};
+        return networkList;
+    }
+
+
+    public void connectToNetwork(String network) {
+        System.out.println("Connecting To.." + network);
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        mySmartPhone m = new mySmartPhone();
+        String[] net = m.getNetwork();
+        for (String item:net)
+        {
+            System.out.println(item);
+        }
+
+        m.callNumber(1234567890);
+    }
+}
